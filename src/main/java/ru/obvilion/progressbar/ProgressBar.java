91c8d6d06f94bdad5c25cf6615e27f53e272fc2f
Fatonndev <56699208+Fatonndev@users.noreply.github.com>
2023-06-04 23:40:16 +0300

    chore: new version

--- src/main/java/ru/obvilion/progressbar/ProgressBar.java
+++ src/main/java/ru/obvilion/progressbar/ProgressBar.java
@@ -3,7 +3,7 @@ package ru.obvilion.progressbar;
 import java.io.File;
 
 public class ProgressBar {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static void loadLib(File from) {
         System.load(from.getAbsolutePath());
